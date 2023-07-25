e2ab9f9abe2d1f2308a3a6b828286bc43e1a7ba0
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-07-25 13:00:38 +0600

    modified:   pages/work.html 	modified:   script/work_script.js 	modified:   style/about.css 	modified:   style/contact.css 	modified:   style/skills.css 	modified:   style/work.css

--- script/work_script.js
+++ script/work_script.js
@@ -21,3 +21,45 @@ const typed = new  Typed('.multitext', {
     backDelay: 1000,
     loop: true
 });
+
+
+// JavaScript code
+const videoElement = document.getElementById("video");
+
+// Function to toggle fullscreen mode
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    // If the video is not in fullscreen, make it fullscreen
+    if (videoElement.requestFullscreen) {
+      videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) {
+      videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) {
+      videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) {
+      videoElement.msRequestFullscreen();
+    }
+  } else {
+    // If the video is already in fullscreen, exit fullscreen
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+}
+
+// Add click event listener to the play button
+videoElement.addEventListener("click", function () {
+  if (videoElement.paused) {
+    // If the video is paused, play it and toggle fullscreen
+    videoElement.play().then(() => toggleFullScreen());
+  } else {
+    // If the video is playing, toggle fullscreen directly
+    toggleFullScreen();
+  }
+});
